3331acc73b276619af24e3ccded15e82c588d7d0
kim-eungseo <juniormoo87@gmail.com>
2022-02-14 17:09:54 +0900

    proto

--- main.py
+++ main.py
@@ -1,11 +1,66 @@
-from typing import Optional
-
+from datetime import datetime
 from fastapi import FastAPI
+from pydantic import BaseModel
+from urllib.request import Request, urlopen
+from urllib.parse import quote
 
 app = FastAPI()
+url = "www.sedaily.com/Search/Search/SEList?"
 
 
-@app.get("/search/{keyword}")
-def read_item(keyword: int, kw: Optional[str] = None):
-    return {"keyword": keyword, "kw": kw}
+class Query(BaseModel):
+    page: int
+    scDetail: str
+    scOrdBy: int = 0
+    catView: str = "AL"
+    scText: str
+    scPeriod: str = "6m"
+    scArea: str = "tc"
+    scTextIn: str
+    scTextExt: str
+    scPeriodS: str
+    scPeriodE: str
+    command: str
+    _: int = int(datetime.now().timestamp())
 
+
+@app.put("/v1/search")
+def search(query: Query):
+    paraList = [
+        "Page" + str(query.page),
+        "scDetail" + query.scDetail,
+        "scOrdBy" + str(query.scOrdBy),
+        "catView" + query.catView,
+        "scText" + quote(query.scText),
+        "scPeriod" + query.scPeriod,
+        "scText" + quote(query.scText),
+        "scPeriod" + query.scPeriod,
+        "scArea" + query.scArea,
+        "scTextIn" + quote(query.scTextIn),
+        "scTextExt" + quote(query.scTextExt),
+        "scPeriodS" + query.scPeriodS,
+        "scPeriodE" + query.scPeriodE,
+        "command" + query.command,
+        "_" + int(datetime.now().timestamp())
+    ]
+
+    requestUrl = url + '&'.join(paraList)
+
+    request = Request(requestUrl, headers={'User-Agent': 'Mozilla/5.0'})  # UA가 없으면 403 에러 발생
+
+    return {
+        "Page": query.page,
+        "scDetail": query.scDetail,
+        "scOrdBy": query.scOrdBy,
+        "catView": query.catView,
+        "scText": quote(query.scText),
+        "scPeriod": query.scPeriod,
+        "scArea": query.scArea,
+        "scTextIn": quote(query.scTextIn),
+        "scTextExt": quote(query.scTextExt),
+        "scPeriodS": query.scPeriodS,
+        "scPeriodE": query.scPeriodE,
+        "command": query.command,
+        "_": int(datetime.now().timestamp()),
+        "result": request.read
+    }
